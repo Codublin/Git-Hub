f51416fbb83215ec9f760442657cde2a29886ba9
Thomasina Egan <eganthomasina@icloud.com>
2014-03-10 15:22:44 +0000

    Mark committed

---
+++ ForceController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForceController : MonoBehaviour {
+	{
+
+		Vector3 force = Vector3.zero;
+		Vector3 velocity = Vector3.zero;
+
+		float maxSpeed = 20.0f
+		float mass =1.0f
+
+			void start ()
+			{
+
+			}
+
+		void Update () {
+
+			force += Seek (enemy);
+			Vector3 acc = force / mass;
+
+			velcoity += acc* Time.deltatime;
+			transform.position += velocity*Time.deltatime;
+
+			force = Vector3.zero;
+
+
+
+		}
+
+		Vector3 Seek(GameObject enemy)
+		{
+			Vector3 desiredVelocity = enemy.transform.position - transform.position;
+			dseiredVelocity.Normalize();
+			desiredVelocity*=maxSpeed;
+
+
+		}
+
+
+}
---
+++ enemy.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemy : 
+{
+	Public Vector3 force = Vector3.zero;
+	Public Vector3 velocity = Vector3.zero;
+
+
+
+		
+		
+		
+		float maxSpeed = 20.0f
+			float mass = 0.0f
+				
+				
+				void update ()
+			{
+				force +=Seek (target)
+				Vector3 acceleration = force/mass;
+				velocity +=acceleration * Time.deltatime
+				transform.position=velocity*Time.deltatime
+				
+				force=Vector3.zero
+				if(velocity.magnitude> float.eplison);
+			}
+		{
+			Transform.forward=velocity.normalize()
+		}
+		
+	}
